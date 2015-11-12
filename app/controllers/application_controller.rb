398349e6b9f4a963abad863ca27aafe33116dbde
p1204713 <p1204713@stud.ipm.edu.mo>
2015-11-12 18:46:33 +0000

    Add Yo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def Yo
+    render text: "YO YO YO ~~"
+  end
 end
